9026923aee6441e5ef3ecae0df14cf83451009c7
unknown <khalil.mouteirek@gmail.com>
2024-07-07 22:19:10 +0300

    modified client file as needed

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 import yaml
+import os
 
 def load_config():
-    with open('../config.yaml', 'r') as file:
+    config_path = os.path.join(os.path.dirname(__file__), '../config.yaml')
+    with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
         return config
 
